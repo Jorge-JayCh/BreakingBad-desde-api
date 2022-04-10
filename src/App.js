f7f0094cd1be755ad56772ef65bddcb80911a01e
Jorge Andrés Choque <jorge.ch.999santos@gmail.com>
2022-04-10 14:02:29 -0300

    Documentacion Agregada

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import Frase from './components/Frase';
 
@@ -15,9 +15,15 @@ const Boton = styled.button`
   font-family: Arial, Helvetica, sans-serif;
   color: #FFF;
   margin-top: 3rem;
-  padding: 1rem 3rem;
+  padding: 1rem 2rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
@@ -27,11 +33,17 @@ function App() {
 
 
   const consultarAPI =  async () => {
-    console.log('consultando ... =)');
+    // console.log('consultando ... =)');
     const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
     const frase = await api.json();
     guardarFrase( frase[0] );
   }
+
+  // Cargar una frase
+  useEffect(() => {
+    consultarAPI();
+  },[]);
+
 
   return (
     <Contenedor>
